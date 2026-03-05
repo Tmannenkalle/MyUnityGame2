cc10d373d8d7d12e22497073030f677187d65362
selstomarcus-gif <selstomarcus@gmail.com>
2026-03-05 19:20:18 +0100

    kiste og  begynte på interaction system

---
+++ MyUnityGame2/Assets/Interaction.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    [SerializeField] private Transform interactionPoint;
+    [SerializeField] private float interactionPointRadius = 0.5f;
+}
