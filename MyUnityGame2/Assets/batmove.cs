54506f5fc679ca28f9ea16ed1a1695706f461e42
Tmannenkalle <monke26890@gmail.com>
2026-03-11 15:31:32 +0100

    pls virk uwu

---
+++ MyUnityGame2/Assets/batmove.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class batmove : MonoBehaviour
+{
+    private float move = 1f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Untagged"));
+    }
+}
