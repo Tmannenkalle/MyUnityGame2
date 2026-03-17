28b46ccefdade87f0f72ad0f6eab81f8e352c6a7
selstomarcus-gif <selstomarcus@gmail.com>
2026-03-17 18:06:01 +0100

    Snart FERDIG lets gOOOooOOO
    jeg er snart ferdig med interaction

--- MyUnityGame2/Assets/Scripts/InteractionNpc.cs
+++ MyUnityGame2/Assets/Scripts/InteractionNpc.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
 using TMPro;
+using System.IO.Pipes;
+using Microsoft.VisualBasic;
 
-public class InteractionNpc : MonoBehaviour
+public class InteractionNpc : MonoBehaviour, IInteractable
 {
     //må gjøre dette for å fikse error, bytt det tilbake når du må
-    //public NPCDialogue dialogueData;
+    public Interaction dialogueData;
     public GameObject dialoguePanel;
     public TMP_Text dialougetext, nameText;
     private int dialogeIndex;
     private bool isTyping, isDialogeActive;
+
+    public bool CanInteract()
+    {
+        return !isDialogeActive;
+    }
+    public void Interact()
+    {
+        if(dialogueData == null)
+        {
+            return;
+        }
+        if (isDialogeActive)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
