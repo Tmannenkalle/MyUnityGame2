6d6d0405084657a8c5ede393137d97eef3b34529
selstomarcus-gif <selstomarcus@gmail.com>
2026-06-07 11:27:52 +0200

    Rework med knapper

--- MyUnityGame2/Assets/Scripts/Click.cs
+++ MyUnityGame2/Assets/Scripts/Click.cs
@@ -1,11 +1,16 @@
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 using UnityEngine.AdaptivePerformance;
+using UnityEngine.Rendering;
 
 public class Click : MonoBehaviour
 {
     public GameObject per;
 
-    public bool on_button;
+    public bool on_button1;
+    public bool on_button2;
+
+    public CameraFollow cf;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,13 +20,38 @@ public class Click : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       per.transform.position = Input.mousePosition;
+        Vector3 mousepositon = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepositon.z = 0f;
+       per.transform.position = mousepositon;
+       if(on_button1 && Input.GetMouseButtonDown(0))
+        {
+            cf.IntUp();
+        }
+        if(on_button2 && Input.GetMouseButtonDown(0))
+        {
+            cf.started = true;
+        }
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Button"))
         {
-            
+            on_button1 = true;
+        }
+        if(collision.gameObject.CompareTag("Button2"))
+        {
+            on_button2 = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Button"))
+        {
+            on_button1 = false;
+        }
+        if(collision.gameObject.CompareTag("Button2"))
+        {
+            on_button2 = false;
         }
     }
 
